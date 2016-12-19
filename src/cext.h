7830a62ba0d355921c75d35ec7e9e1574d2a243a
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2016-12-19 21:32:13 +0900

    support non-Linux platform.

--- src/cext.h
+++ src/cext.h
@@ -1,3 +1,5 @@
-#include <sys/inotify.h>
+#if defined( __linux__ )
+#	include <sys/inotify.h>
+#endif
 #include <jack/jack.h>
 #include <jack/midiport.h>
--- src/notify.rs
+++ src/notify.rs
@@ -1,11 +1,12 @@
 // by Yasuhiro Fujii <y-fujii at mimosa-pudica.net>, under 2-clause BSD license.
 use std::*;
-use std::io::prelude::*;
-use std::os::unix::io::FromRawFd;
-use cext;
 
 
+#[cfg( target_os = "linux" )]
 pub fn notify_wait( path: &str ) -> io::Result<()> {
+	use cext;
+	use std::io::prelude::*;
+	use std::os::unix::io::FromRawFd;
 	unsafe {
 		let fd = cext::inotify_init1( cext::IN_CLOEXEC as os::raw::c_int );
 		let mut fs = fs::File::from_raw_fd( fd );
@@ -20,6 +21,28 @@ pub fn notify_wait( path: &str ) -> io::Result<()> {
 
 		let mut buf: [u8; 4096] = mem::uninitialized();
 		let _ = fs.read( &mut buf )?; // XXX
+	}
+	Ok( () )
+}
+
+#[cfg( not( target_os = "linux" ) )]
+pub fn notify_wait( path: &str ) -> io::Result<()> {
+	let bgn = fs::metadata( path )?.modified()?;
+	let mut mid;
+	loop {
+		mid = fs::metadata( path )?.modified()?;
+		if mid != bgn {
+			break;
+		}
+		thread::sleep( time::Duration::from_millis( 100 ) );
+	}
+	loop {
+		thread::sleep( time::Duration::from_millis( 100 ) );
+		let end = fs::metadata( path )?.modified()?;
+		if end == mid {
+			break;
+		}
+		mid = end;
 	}
 	Ok( () )
 }

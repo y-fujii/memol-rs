e8a7ee8ef50744384135320f271cc5f60018b4fd
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2017-12-29 10:49:07 +0900

    fix window size.

--- memol_gui/src/imutil.rs
+++ memol_gui/src/imutil.rs
@@ -173,6 +173,7 @@ pub fn set_theme( base: ImVec4, fg: ImVec4, bg: ImVec4 ) {
 	style.ChildRounding  = 0.0;
 	style.PopupRounding  = 0.0;
 	style.FrameRounding  = 0.0;
+	style.WindowMinSize  = ImVec2::zero();
 	style.Colors[ImGuiCol_Text                 as usize] = fg;
 	style.Colors[ImGuiCol_Border               as usize] = normal;
 	style.Colors[ImGuiCol_WindowBg             as usize] = bg;

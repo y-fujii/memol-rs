66b5932f43caa6e3c6250ddc3a3ce0879068b66d
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2018-03-04 00:17:06 +0900

    cleanup.

--- memol_vst/src/events.rs
+++ memol_vst/src/events.rs
@@ -31,6 +31,20 @@ impl EventBuffer {
 			}
 		};
 		for i in 0 .. BUFFER_SIZE {
+			(*this.holder)[i] = vst::api::MidiEvent{
+				event_type: vst::api::EventType::Midi,
+				byte_size: mem::size_of::<vst::api::MidiEvent>() as i32,
+				delta_frames: 0,
+				flags: 0,
+				note_length: 0,
+				note_offset: 0,
+				midi_data: [ 0, 0, 0 ],
+				_midi_reserved: 0,
+				detune: 0,
+				note_off_velocity: 0,
+				_reserved1: 0,
+				_reserved2: 0,
+			};
 			this.buffer.events[i] = &mut (*this.holder)[i];
 		}
 		this
@@ -41,20 +55,11 @@ impl EventBuffer {
 	}
 
 	pub fn push( &mut self, msg: &[u8], frame: i32 ) {
-		self.holder[self.buffer.num_events as usize] = vst::api::MidiEvent{
-			event_type: vst::api::EventType::Midi,
-			byte_size: mem::size_of::<vst::api::MidiEvent>() as i32,
-			delta_frames: frame,
-			flags: 0,
-			note_length: 0,
-			note_offset: 0,
-			midi_data: [ msg[0], msg[1], msg[2] ],
-			_midi_reserved: 0,
-			detune: 0,
-			note_off_velocity: 0,
-			_reserved1: 0,
-			_reserved2: 0,
-		};
+		let ev = &mut self.holder[self.buffer.num_events as usize];
+		ev.delta_frames = frame;
+		for i in 0 .. 3 {
+			ev.midi_data[i] = msg[i];
+		}
 		self.buffer.num_events += 1;
 	}
 
